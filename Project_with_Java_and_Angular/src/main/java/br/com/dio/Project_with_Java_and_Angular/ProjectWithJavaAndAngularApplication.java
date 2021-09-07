661015e5363b80ce2d8801706da1f65a81a13068
andrerj2000 <andrerj2000@gmail.com>
2021-09-07 10:52:02 -0300

    Start project and Java with Angular

---
+++ Project_with_Java_and_Angular/src/main/java/br/com/dio/Project_with_Java_and_Angular/ProjectWithJavaAndAngularApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.Project_with_Java_and_Angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWithJavaAndAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWithJavaAndAngularApplication.class, args);
+	}
+
+}
---
+++ Project_with_Java_and_Angular/src/test/java/br/com/dio/Project_with_Java_and_Angular/ProjectWithJavaAndAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.Project_with_Java_and_Angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWithJavaAndAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
